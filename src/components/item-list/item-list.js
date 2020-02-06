bc9c01ae110a1bf203181981dd56b8f6e926003a
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-02-06 22:31:39 +0300

    feat: use children in itemDetails

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,46 +1,52 @@
 import React, { Component } from 'react';
 
 import './item-list.css';
-
-import SwapiService from '../../services/swapi-service';
-import Spinner from '../spinner';
+import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner/spinner";
 
 export default class ItemList extends Component {
 
-  swapiService = new SwapiService();
-
   state = {
-    peopleList: null,
+    itemList: null
   };
 
   componentDidMount() {
-    this.swapiService
-      .getAllPeople()
-      .then((peopleList) => {
+
+    const { getData } = this.props;
+
+    getData()
+      .then((itemList) => {
         this.setState({
-          peopleList: peopleList,
+          itemList
         });
       });
   }
 
   renderItems(arr) {
-    return arr.map((item) => (
-      <li className="list-group-item"
-        key={item.id}
-        onClick={() => this.props.onItemSelected(item.id)}
-      >
-        {item.name}
-      </li>
-    ));
+    return arr.map((item) => {
+      const { id } = item;
+      const label = this.props.children(item);
+
+      return (
+        <li className="list-group-item"
+            key={id}
+            onClick={() => this.props.onItemSelected(id)}>
+          {label}
+        </li>
+      );
+    });
   }
 
   render() {
 
-    const { peopleList } = this.state;
-    if (!peopleList) {
+    const { itemList } = this.state;
+
+    if (!itemList) {
       return <Spinner />;
     }
-    const items = this.renderItems(peopleList);
+
+    const items = this.renderItems(itemList);
+
     return (
       <ul className="item-list list-group">
         {items}
